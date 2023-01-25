0f2e9bff6b4f4f5630325522d40e68ad4ef63034
Richard Bown <rich@xyglo.com>
2023-01-25 22:29:14 +0100

    Added select class

--- whizzo-worker/whizzo-worker/src/main/java/App.java
+++ whizzo-worker/whizzo-worker/src/main/java/App.java
@@ -1,9 +1,18 @@
 import static spark.Spark.*;
+import mariadb.*;
 
 public class App {
+
+    //private mariadb.MariaDBSelect selector = new mariadb.MariaDBSelect();
+
     public static void main(String[] args) {
+
         port(8080);
 
         get("/", (req, res) -> "Hello from Whizzo Worker!");
+
+        //mariadb.MariaDBSelect.connect();
+
+//        get("/select", (req, res) -> );
     }
 }
