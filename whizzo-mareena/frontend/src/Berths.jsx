205a321c925d62407d2c87ff150f292cba895674
Richard Bown <rich@xyglo.com>
2023-04-11 15:03:37 +0200

    Halted worker

--- whizzo-mareena/frontend/src/Berths.jsx
+++ whizzo-mareena/frontend/src/Berths.jsx
@@ -12,37 +12,52 @@ const columns = [
     dataIndex: 'berth_id',
     key: 'id',
     width: 30,
+    sorter: (a, b) => a.id.localeCompare(b.id)
   },
   {
     title: 'Berth Name',
     dataIndex: 'berth_name',
     key: 'berth_name',
     width: 100,
+    sorter: (a, b) => a.title.localeCompare(b.title)
   },
   {
     title: 'Boat Name',
     dataIndex: 'boat_name',
     key: 'boat_name',
     width: 100,
+    sorter: (a, b) => a.boat_name.localeCompare(b.boat_name),
+    render: (text) => {
+      return text || "<empty>";
+    }
   },
   {
     title: 'Customer Name',
     dataIndex: 'customer_name',
     key: 'customer_name',
     width: 100,
+    sorter: (a, b) => a.customer_name.localeCompare(b.customer_name),
+    render: (text) => {
+      return text || '<not assigned>';
+    }
   },
   {
     title: 'Pontoon',
     dataIndex: 'pontoon_name',
     key: 'pontoon_name',
-    width: 100
+    width: 100,
+    sorter: (a, b) => a.pontoon_name.localeCompare(b.pontoon_name),
   },
 ];
 
 function Berths(){
 
   const [data, setData] = useState([])
-  
+  const [sortOrder, setSortOrder] = useState({});
+  const handleSort = (pagination, filters, sorter) => {
+    setSortOrder(sorter);
+  };
+
   useEffect(() => {
 
     async function fetchBerths() {
@@ -78,7 +93,10 @@ function Berths(){
   return (
     <div className="App-header">
       <h1>Berths</h1>
-      <Table columns={columns} data={data} rowKey='id'/>
+      <Table columns={columns} data={data} rowKey='berth_id' onChange={handleSort}
+      pagination={false}
+      sortOrder={sortOrder.order}
+      sortColumns={sortOrder.columnKey}/>
     </div>
     );
 }
